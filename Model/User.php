28e2b2b7029dba11788ffe5e98d39b8943dfc029
Simo Heinonen <simo@dealdash.com>
2023-12-13 15:48:21 +0200

    Allow symfony 7

--- Model/User.php
+++ Model/User.php
@@ -180,10 +180,7 @@ abstract class User implements UserInterface, EquatableInterface, \Serializable
         return $this;
     }
 
-    /**
-     * @return void
-     */
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
         $this->plainPassword = null;
     }
